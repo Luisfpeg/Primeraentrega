e7c40fb4e9d1b48d4fe969fe7e2b890dbb849805
Luis Fernando Peña Garcia <115048767+Luisfpeg@users.noreply.github.com>
2023-03-13 17:37:10 -0500

    Push

--- src/Componentes/navbar/navbar.js
+++ src/Componentes/navbar/navbar.js
@@ -4,25 +4,19 @@ import Navbar from './components/Navbar';
 
 
 const App = () => {
-  return (
-    <Router>
-      <Navbar />
-      <Switch>
-        <Route path="/productos">
-          {/* Componente de productos */}
-        </Route>
-        <Route path="/carrito">
-          {/* Componente de carrito */}
-        </Route>
-        <Route path="/contacto">
-          {/* Componente de contacto */}
-        </Route>
-        <Route path="/">
-          {/* Componente de inicio */}
-        </Route>
-      </Switch>
-    </Router>
-  );
+    return ( <
+        Router >
+        <
+        Navbar / >
+        <
+        Switch >
+
+        <
+        Route path = "/productos" > { productos } < /Route>  <
+        Route path = "/carrito" > { carrito } < /Route>  <
+        Route path = "/contacto" > { contacto } < /Route> <
+        Route path = "/" > { inicio } < /Route> </Switch > < /Router>
+    );
 };
 
 export default App;
