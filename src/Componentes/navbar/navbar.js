a9d5fef8e589cd76c9c9dc195abf25115af84f7d
Luis Fernando Peña Garcia <115048767+Luisfpeg@users.noreply.github.com>
2023-03-13 16:53:07 -0500

    PRUEBA

---
+++ src/Componentes/navbar/navbar.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+
+const App = () => {
+  return (
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route path="/productos">
+          {/* Componente de productos */}
+        </Route>
+        <Route path="/carrito">
+          {/* Componente de carrito */}
+        </Route>
+        <Route path="/contacto">
+          {/* Componente de contacto */}
+        </Route>
+        <Route path="/">
+          {/* Componente de inicio */}
+        </Route>
+      </Switch>
+    </Router>
+  );
+};
+
+export default App;
