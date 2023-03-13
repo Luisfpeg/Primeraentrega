6b1e32038e9661692179f0a779652f3cc144778b
Luis Fernando Peña Garcia <115048767+Luisfpeg@users.noreply.github.com>
2023-03-13 16:41:23 -0500

    pRUEBA

--- src/App.js
+++ src/App.js
@@ -5,6 +5,33 @@ import { BrowserRouter as Router } from "react-router-do";
 import {Paginas} from "./Componentes/Paginas";
 import {DataProvider} from "./context/DataProvider"
 import{ Carrito} from ".Componentes/Carrito";
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+
+const App = () => {
+  return (
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route path="/productos">
+          {/* Componente de productos */}
+        </Route>
+        <Route path="/carrito">
+          {/* Componente de carrito */}
+        </Route>
+        <Route path="/contacto">
+          {/* Componente de contacto */}
+        </Route>
+        <Route path="/">
+          {/* Componente de inicio */}
+        </Route>
+      </Switch>
+    </Router>
+  );
+};
+
+export default App;
 
 function App() {
   return (
